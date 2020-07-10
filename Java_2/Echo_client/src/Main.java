578a3f0aa8ecb90e81efdd1edb8051e906d1b479
sablist99 <53463825+sablist99@users.noreply.github.com>
2020-07-10 23:19:11 +0700

    /

---
+++ Java_2/Echo_client/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    public static void main ( String [] args ) {
+        new MyWindow ();
+    }
+}
